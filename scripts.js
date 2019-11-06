18d026ef69895e8edecb14ace23eff95936cd1d3
BinMz <46683950+binmz@users.noreply.github.com>
2019-11-07 05:22:26 +0700

    1.0

--- scripts.js
+++ scripts.js
@@ -2,8 +2,9 @@ $(document).ready(function () {
     //selector
     //event
     //animate
-    $('.addBtn').click(function(event){
-        $('#form-add').animate({marginLeft: 0, opacity: 0});
-        $('#form-add').animate({marginLeft: 300, opacity: 1});
+    $('.addBtn').click(function (event) {
+        $('#form-add').animate({ marginLeft: 0, opacity: 0 });
+        $('#form-add').animate({ marginLeft: 300, opacity: 1 });
     });
+    $(".addBtn")
 });
