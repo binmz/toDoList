4effebce6bc94ea88318f7930363b3fac6063a36
Hữu Hà ZSmart <40519768+huuhait@users.noreply.github.com>
2020-01-23 22:03:53 +0700

    rebase code

--- scripts.js
+++ scripts.js
@@ -1,14 +1,34 @@
-$(document).ready(function () {
-    $("#addBtn").click(function () {
-        var ti = new Date();
-        var time = ti.getHours() + ":" + ti.getMinutes() + ":" + ti.getSeconds();
-        var vietra = ('<td>' + time + '</td>');
-        $("table").append('<tr>' + vietra + '</tr>');
-        var strDate = ti.getDay() + "/" + (ti.getMonth() + 1) + "/" + ti.getFullYear();
-        $("table tr:last-child").append('<td>' + strDate + '</td>');
-        $("table td").append('<td>' + $("#myInput").val() + '</td>');
-        $("tabble td").append('<td>' + $(".check").text() + '</td>');
+$(document).ready(function() {
+  const TODO_LIST = []; // { time_created: xxx, title, detail }
+
+  $("#addBtn").click(function() {
+    const time_created = new Date();
+    const title = $(".input-title").val();
+    const details = $(".input-details").val();
+
+    TODO_LIST.push({
+      time_created,
+      title,
+      details
     });
+
+    $("table tbody").append(`
+        <tr class="todo-row">
+            <td>${time_created}</td>
+            <td>${title}</td>
+            <td>${details}</td>
+            <td>
+                <button>EDIT</button>
+            </td>
+        </tr>
+    `);
+
+    $(".todo-row > td > button").unbind();
+    $(".todo-row > td > button").click(function() {
+      const table_row = this.closest("tr");
+      const row_key = table_row["rowIndex"];
+
+      console.log(TODO_LIST[row_key - 1]);
+    });
+  });
 });
-
-
