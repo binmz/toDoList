1725dd11e8ad2941f098391e5cc4cc8a81d0ba2f
BinMz <46683950+binmz@users.noreply.github.com>
2020-01-23 17:18:25 +0700

    todolist 1.1

--- scripts.js
+++ scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function () {
-    //selector
-    //event
-    //animate
-
+    $("#addBtn").click(function () {
+        var ti = new Date();
+        var time = ti.getHours() + ":" + ti.getMinutes() + ":" + ti.getSeconds();
+        var vietra = ('<td>' + time + '</td>');
+        $("table").append('<tr>' + vietra + '</tr>');
+        var strDate = ti.getDay() + "/" + (ti.getMonth() + 1) + "/" + ti.getFullYear();
+        $("table tr:last-child").append('<td>' + strDate + '</td>');
+        $("table td").append('<td>' + $("#myInput").val() + '</td>');
+        $("tabble td").append('<td>' + $(".check").text() + '</td>');
+    });
 });
+
+
