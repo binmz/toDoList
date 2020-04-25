665ef2b937b9a722ee9b85c22eac40101908407e
BinMz <46683950+binmz@users.noreply.github.com>
2020-04-26 01:41:58 +0700

    Vuejs

--- scripts.js
+++ scripts.js
@@ -1,16 +1,10 @@
-$(document).ready(function() {
-  const TODO_LIST = []; // { time_created: xxx, title, detail }
+/*$(document).ready(function () {
+  // { time_created: xxx, title, detail }
 
-  $("#addBtn").click(function() {
+  $("#addBtn").click(function () {
     const time_created = new Date();
     const title = $(".input-title").val();
     const details = $(".input-details").val();
-
-    TODO_LIST.push({
-      time_created,
-      title,
-      details
-    });
 
     $("table tbody").append(`
         <tr class="todo-row">
@@ -18,17 +12,26 @@ $(document).ready(function() {
             <td>${title}</td>
             <td>${details}</td>
             <td>
-                <button>EDIT</button>
+                <button>DELETE</button>
             </td>
         </tr>
     `);
 
-    $(".todo-row > td > button").unbind();
-    $(".todo-row > td > button").click(function() {
-      const table_row = this.closest("tr");
-      const row_key = table_row["rowIndex"];
 
-      console.log(TODO_LIST[row_key - 1]);
-    });
   });
 });
+*/
+var addBtn = new Vue({
+  el: '.addTodo',
+  methods: {
+    say: function (message) {
+      alert(message)
+    }
+  }
+});
+var addTodo = new Vue({
+  el: 'tbody',
+  methods: {
+
+  },
+})
